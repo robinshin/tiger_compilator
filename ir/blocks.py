ae32a4cc5cb1c08dd187168322e2901bc51356d4
Samuel Tardieu <sam@rfc1149.net>
2017-03-29 18:14:47 +0200

    Call reorder_blocks during canonicalization phase

---
+++ ir/blocks.py
@@ -0,0 +1,12 @@
+from ir.nodes import *
+from frame.frame import Frame
+
+
+def reorder_blocks(seq, frame):
+    """Reorder blocks in seq so that the negative branch of a CJUMP always
+    follows the CJUMP itself. frame is the frame of the corresponding
+    function."""
+    assert(isinstance(seq, SEQ))
+    assert(isinstance(frame, Frame))
+    # Implement here
+    return seq
