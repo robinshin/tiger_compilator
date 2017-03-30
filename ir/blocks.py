10304770136bffc50be992aceee4851eab285fbe
Robin SHIN <robin.shin@telecom-paristech.fr>
2017-03-31 01:35:28 +0200

    Some corrections of ir/blocks.py

--- ir/blocks.py
+++ ir/blocks.py
@@ -23,31 +23,32 @@ def reorder_blocks(seq, frame):
     examinated_blocks = blocks[0]
     del blocks[0]
     while len(blocks) != 0:
-        current_block = examinated_blocks[-1][-1].target
-        if isinstance(current_block, JUMP):
+        current_block_type = examinated_blocks[-1]
+        if isinstance(current_block_type, JUMP):
+            current_block = current_block_type.target
             if current_block in blocks:
                 examinated_blocks.append(current_block)
                 blocks.remove(current_block)
             else:
                 examinated_blocks.append(blocks[0])
                 del blocks[0]
-        elif isinstance(current_block, CJUMP):
-            true_block = current_block.ifTrue
-            false_block = current_block.ifFalse
+        elif isinstance(current_block_type, CJUMP):
+            true_block = current_block_type.ifTrue
+            false_block = current_block_type.ifFalse
             if false_block in blocks:
                 examinated_blocs.append(false_block)
                 blocks.remove(false_block)
             elif true_block in blocks:
-                right_tmp = current_block.right
-                current_block.right = current_block.left
-                current_block.left = right_tmp
-                current_block.ifTrue = false_block
-                current_block.ifFalse = true_block
+                right_tmp = current_block_type.right
+                current_block_type.right = current_block_type.left
+                current_block_type.left = right_tmp
+                current_block_type.ifTrue = false_block
+                current_block_type.ifFalse = true_block
                 examinated_blocks.append(true_block)
                 blocks.remove(true_block)
             else:
-                examinated_blocks.append([new_label, JUMP(false_black.label)])
-                current_block.ifFalse.label = new_label
+                examinated_blocks.append([Label("new_label"), JUMP(NAME(false_block.label))])
+                current_block_type.ifFalse.label = Label("new_label")
 
 
     return seq
